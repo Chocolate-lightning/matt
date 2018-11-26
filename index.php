b12cde2e11a1e36fce233e73b25c5cd6e0a9be39
Mathew May <mathew@MatMac.local>
2018-11-26 10:45:15 +0800

    Second release

--- index.php
+++ index.php
@@ -15,13 +15,23 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for tool_matt.
- *
  * @package tool_matt
  * @copyright 2018 Mathew May mathew@moodle.com
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'My nth Moodle plugin';
-$string['hello'] = 'Hello world';
-$string['course'] = 'The course id is {$a}';
+require_once(__DIR__ . '/../../../config.php');
+$url = new moodle_url('/admin/tool/matt/index.php');
+$PAGE->set_context(context_system::instance());
+$PAGE->set_url($url);
+$PAGE->set_pagelayout('report');
+$PAGE->set_title('Hello to the matt list');
+$PAGE->set_heading(get_string('pluginname', 'tool_matt'));
+
+$id = optional_param('id', 0, PARAM_INT);
+
+echo $OUTPUT->header();
+
+echo html_writer::tag('p', get_string('hello', 'tool_matt'));
+
+echo html_writer::tag('p', get_string('course', 'tool_matt', $id));
--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018112600;
+$plugin->version = 2018112601;
 $plugin->requires  = 2018111800;
 $plugin->component = 'tool_matt';
-$plugin->release = '1.0';
+$plugin->release = '1.2';
