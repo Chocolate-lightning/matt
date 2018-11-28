acb9b2f5b09c67213a91d9387db575911e3ebfa7
Mathew <mathewm@hotmail.co.nz>
2018-11-28 16:12:55 +0800

    Initial PHPUnit file

---
+++ tests/simple_test.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tool Matt tests.
+ *
+ * @package    tool_matt
+ * @category   phpunit
+ * @copyright  2018 Mathew May {@link http://mathew.solutions}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+
+/**
+ * Tool Matt tests.
+ *
+ * @group      tool_matt
+ * @package    tool_matt
+ * @category   phpunit
+ * @copyright  2018 Mathew May {@link http://mathew.solutions}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class tool_matt_simple_testcase extends advanced_testcase {
+    public function test_adding() {
+        $this->assertEquals(3, 1+2);
+    }
+
+    public function test_deleting() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $DB->delete_records('user');
+        $this->assertEmpty($DB->get_records('user'));
+    }
+
+    public function test_user_table_was_reset() {
+        global $DB;
+        $this->assertEquals(2, $DB->count_records('user', array()));
+    }
+}
